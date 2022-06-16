5765d885972b44ca27fb5ad2679240551fa60a94
mmt9724 <sarvesh.shetty@go-mmt.com>
2022-06-16 17:08:51 +0530

    Upto Product functionality

---
+++ store/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'price', 'category', 'stock', 'modified_date', 'is_available')
+    prepopulated_fields = {'slug': ('product_name',)}
+
+
+admin.site.register(Product, ProductAdmin)
